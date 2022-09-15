e382394cadf72052fe5695b6db6f8ebdee67a471
Lucas Martins Prestini <37966787+Prestini@users.noreply.github.com>
2022-09-15 03:05:41 -0300

    Added Pieces

--- src/game.py
+++ src/game.py
@@ -1,10 +1,11 @@
 import pygame
+from board import Board
 from const import *
 
 class Game:
 
     def __init__(self) -> None:
-        pass
+        self.board = Board()
 
     def show_bg(self, surf) -> None:
         for row in range(ROWS):
@@ -17,3 +18,15 @@ class Game:
                 rect = (col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE)
 
                 pygame.draw.rect(surf, color, rect)
+    
+    def show_pieces(self, surf):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # Check if there's a piece
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQ_SIZE + SQ_SIZE // 2, row * SQ_SIZE + SQ_SIZE // 2
+                    piece.texture_rect = img.get_rect(center = img_center)
+                    surf.blit(img, piece.texture_rect)
--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ class Main:
         while True:
 
             self.game.show_bg(self.screen)
+            self.game.show_pieces(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
